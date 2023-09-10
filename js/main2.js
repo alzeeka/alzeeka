cc27e3e9f3cf49abd9e3533849de87503b69405d
alzeeka <142642851+alzeeka@users.noreply.github.com>
2023-09-10 22:30:14 +0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -66,7 +66,7 @@ $(document).ready(function() {
       document.cookie = 'username=' + username + '; expires=30; path=/';
       window.location.href = 'c.html';
     }
-    else if ((username === 'hail2023' && password === 'java') || (username === 'hhh' && password === '123')) {
+    else if (username === 'hail2023' && password === 'java') {
       // إنشاء ملف تعريف الارتباط
       document.cookie = 'username=' + username + '; expires=30; path=/';
       window.location.href = 'java_hail.html';
