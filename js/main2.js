daf7b54d6a760d4755bdc3cf83ffe016c677eb93
alzeeka <142642851+alzeeka@users.noreply.github.com>
2024-01-17 10:09:02 +0300

    Update main2.js

--- js/main2.js
+++ js/main2.js
@@ -4,35 +4,15 @@
 
 
 $(document).ready(function() {
-   var videoLinks = ["https://www.youtube.com/embed/Bne2jPhQMrc?si=LdeR80yKfrsRrxDS",
-  "https://www.youtube.com/embed/wOMxpwWlf3U?si=rxGtOG5nHCTyZIiy",
-  "https://www.youtube.com/embed/79EgaXZ3grc?si=guEWsm6grAOyBWr4",
-  "https://www.youtube.com/embed/wB5Qv7fbMmI?si=uz5DKFTRZcwddt4t",
-  "https://www.youtube.com/embed/64gWpJ_I_pk?si=hJRLQckwRzpSjXkH",
-  "https://www.youtube.com/embed/ydlm0P4yFeQ?si=e2nv5T_00qvUVleU"];
+   var videoLinks = ["https://www.youtube.com/embed/gYlCQv8b8Dw?si=7B66tGjQBnY5vLql"];
 
 
-  var videoNames = [ " شرح خطة المقرر ",
-  " lectuer 1.1 | Components of a Personal Computer ",
-  " lectuer 1.2 | What is programming ",
-  " lectuer 1.3 | Java and programming language",
-  " lectuer 1.4 | Java program ",
-  " كتابة اول برنامج عملي"];
+  var videoNames = [ " شرح عملي | Encapsulation "];
 
-  var videoLinks1 = ["https://www.youtube.com/embed/Bne2jPhQMrc?si=LdeR80yKfrsRrxDS",
-  "https://www.youtube.com/embed/wOMxpwWlf3U?si=rxGtOG5nHCTyZIiy",
-  "https://www.youtube.com/embed/79EgaXZ3grc?si=guEWsm6grAOyBWr4",
-  "https://www.youtube.com/embed/wB5Qv7fbMmI?si=uz5DKFTRZcwddt4t",
-  "https://www.youtube.com/embed/64gWpJ_I_pk?si=hJRLQckwRzpSjXkH",
-  "https://www.youtube.com/embed/ydlm0P4yFeQ?si=e2nv5T_00qvUVleU"];
+  var videoLinks1 = ["https://www.youtube.com/embed/gYlCQv8b8Dw?si=7B66tGjQBnY5vLql"];
 
 
-  var videoNames1 = [ " شرح خطة المقرر ",
-  " lectuer 1.1 | Components of a Personal Computer ",
-  " lectuer 1.2 | What is programming ",
-  " lectuer 1.3 | Java and programming language",
-  " lectuer 1.4 | Java program ",
-  " كتابة اول برنامج عملي"];
+  var videoNames1 = [ " شرح عملي | Encapsulation "];
 
 
   var videoLinks2 = [];
@@ -62,18 +42,18 @@ $(document).ready(function() {
 //###########################################################################################################
 //###########################################################################################################
 
- var AvideoLinks = ["https://www.youtube.com/embed/Bne2jPhQMrc?si=LdeR80yKfrsRrxDS"
+ var AvideoLinks = ["https://www.youtube.com/embed/gYlCQv8b8Dw?si=7B66tGjQBnY5vLql"
 ];
 
 
 var AvideoNames = [ 
-"  كتابة اول برنامج عملي "
+" شرح عملي | Encapsulation "
 ];
 
-var AvideoLinks1 = ["https://www.youtube.com/embed/Bne2jPhQMrc?si=LdeR80yKfrsRrxDS"];
+var AvideoLinks1 = ["https://www.youtube.com/embed/gYlCQv8b8Dw?si=7B66tGjQBnY5vLql"];
 
 
-var AvideoNames1 = [ "  كتابة اول برنامج عملي " ];
+var AvideoNames1 = [ " شرح عملي | Encapsulation " ];
 
 
 var AvideoLinks2 = [];
@@ -180,12 +160,12 @@ var HvideoNames = [];
 //###########################################################################################################
 //###########################################################################################################
 
- var FileLinks = ["https://drive.google.com/drive/folders/1D2xgmTF2xCWB5sVN9YZfZXfg2UFKBfV-?usp=drive_link",
-"pdf/CSCE101/L1M.pdf"];
+ var FileLinks = ["https://drive.google.com/drive/folders/1WCYXJoXEJccJ2AvM1gPiJI4zbExhvIYX?usp=sharing",
+"pdf/CSCE102/Encapsulision.pdf"];
 
 
 var FileNames = [ " جميع الملفات ",
-" ملخص محاضرة 1 "];
+" Encapsulation ملخص "];
 
 
 
@@ -270,54 +250,56 @@ var FileNames = [ " جميع الملفات ",
  
 var videoRow = document.getElementById("videoRow");
 
-for (var i = 0; i < videoLinks.length; i++) {
-  var link = videoLinks[i];
 
-  var colDiv = document.createElement("div");
-  colDiv.className = "col-xl-4 col-xxl-4 col-lg-6 col-md-6 text-center";
 
-  var cardDiv = document.createElement("div");
-  cardDiv.className = "card";
-  cardDiv.style.boxShadow="    #6a1b9a63 9px 9px 14px 2px";
 
-  var iframe = document.createElement("iframe");
-  iframe.style.aspectRatio = 16/9;
- 
-  iframe.src = link;
-  iframe.title = "YouTube video player";
-  iframe.frameborder = "0";
-  iframe.allow = "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share;fullscreen";
-  iframe.allowfullscreen = true; // تم تفعيل ملء الشاشة هنا
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
   
-  var cardBodyDiv = document.createElement("div");
-  cardBodyDiv.className = "card-body";
-
-  var heading = document.createElement("h3");
-  heading.textContent = videoNames[i];
-
-  // var open = document.createElement("a");
-  // open.style.backgroundColor = "rgb(70, 35, 245)";
-  // open.style.display = "block";
-  // open.style.width = "200px";
-  // open.style.height = "30px";
-  // open.style.lineHeight = "30px";
-  // open.style.color = "white";
-  // open.style.borderRadius = "20px";
-  // open.style.margin = "auto";
-
-  // open.textContent = " فتح ";
-  // open.href = link;
-
-  cardBodyDiv.appendChild(heading);
-  // cardBodyDiv.appendChild(open);
-  cardDiv.appendChild(iframe);
-  cardDiv.appendChild(cardBodyDiv);
-  colDiv.appendChild(cardDiv);
-  videoRow.appendChild(colDiv);
-
- 
-}
 
 
 Files = document.getElementById("Files");
