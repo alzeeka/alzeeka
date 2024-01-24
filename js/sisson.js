4b89aad4875ca4afac6987310e7ada4873483896
alzeeka <142642851+alzeeka@users.noreply.github.com>
2024-01-24 07:39:34 +0300

    Update sisson.js

--- js/sisson.js
+++ js/sisson.js
@@ -6,7 +6,6 @@
 $(document).ready(function() {
 
 
-  
 const ESM = ["9193ce3b31332b03f7d8af056c692b84","11b9e65ecd8eee8c9a545e8ee7f09bdc","4ae3a57b0fb307c3725789e947ecc62c","f7923cb06eb10f2588d597dd4cc35d7c"];
 
 
@@ -30,10 +29,11 @@ $('#loginButton').click(function() {
   for (let i = 0; i < ESM.length; i++) {
     if (hashValue === ESM[i] && password === KLM[i]) {
       document.cookie = 'username=' + ESM[i] + '; expires=30; path=/';
-      window.location.href = `${KLM[i]}VIP.html`;
+      window.location.href = `../${KLM[i]}VIP.html`;
      } 
     
   }
+  
   
 
  
