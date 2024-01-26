d728f544aa9f3a069e83424bc0e9d3ab89cb3108
alzeeka <142642851+alzeeka@users.noreply.github.com>
2024-01-26 09:44:36 +0300

    Update sisson.js

--- Login_v1/js/sisson.js
+++ Login_v1/js/sisson.js
@@ -7,7 +7,7 @@ $(document).ready(function() {
 
 
   
-const ESM = ["9193ce3b31332b03f7d8af056c692b84",cb8205ec3bd898dcc8c0ada1637ad9b7,"11b9e65ecd8eee8c9a545e8ee7f09bdc","4ae3a57b0fb307c3725789e947ecc62c","f7923cb06eb10f2588d597dd4cc35d7c"];
+const ESM = ["9193ce3b31332b03f7d8af056c692b84","cb8205ec3bd898dcc8c0ada1637ad9b7","11b9e65ecd8eee8c9a545e8ee7f09bdc","4ae3a57b0fb307c3725789e947ecc62c","f7923cb06eb10f2588d597dd4cc35d7c"];
 
 
 const KLM = ["CSCE101", "CSCE102","CSCE102","CSCE102","CSCE102"];
