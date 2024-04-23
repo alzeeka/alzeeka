5c29c59b0fc73bb9eafa49b0194e1be3dffc4501
alzeeka <142642851+alzeeka@users.noreply.github.com>
2024-04-23 23:14:14 +0300

    Update sisson.js

--- Login_v1/js/sisson.js
+++ Login_v1/js/sisson.js
@@ -14,13 +14,13 @@ const ESM = ["70a31025536e28676c7985e40d40badf","cd73502828457d15655bbd7a63fb0bc
              "aa19c5fb5b1e63c0e00bbd0704c69754","1d6c95ad833913641be6b34a9306f33e","7e4713beae80078a11ce96f11df1ad1c","85ee8d247374a8f7bb5e1f07a3617453",
              "157fcd7808a342b2ec744ce626ef4a2d","94b3466854b33ee389b78b9f4a6e795d","3df1af7feea931e92e4bd8ef891f2a2b","d7373b9740b8b243ee798d1fd3504905",
              "4d605e048b4c5901f554d33029cf4207","c64489c995349b2b26b446e10f6d0796","cbcb799203788abdd1104ff4c79c6766","fa2a413f3fdc791511abbbc542c499c1",
-            "241cfe97750e3a9dbeadc9e1fc77da00"];
+            "241cfe97750e3a9dbeadc9e1fc77da00","7e63c25a9568fcba975346dc5ac538c2"];
 
 
 const KLM = ["CSCE101","CSCE101", "CSCE102","CSCE102","CSCE102","CSCE102","CSCE102","CSCE102",
              "CSCE102","CSCE102","CSCE101","CSCE101","CSCE102","CSCE102","CSCE102","CSCE101",
              "CSCE102","CSCE102","CSCE102","CSCE101","mid102","CSCE101","CSCE101","CSCE101",
-            "CSCE102","mid101","mid102","mid101","mid102"];
+            "CSCE102","mid101","mid102","mid101","mid102","CSCE101"];
 
 
 
